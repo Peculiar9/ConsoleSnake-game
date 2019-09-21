ec6fb7a847bf871013fa4ff2db3922bb9187079b
Peculiar9 <31594432+Peculiar9@users.noreply.github.com>
2019-09-21 12:08:41 +0100

    Add files via upload

---
+++ snake.cpp
@@ -0,0 +1,140 @@
+// This is a snake game in cee plus plus
+
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;	
+bool gameOver;
+const int width = 20;	
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+void Setup()
+{   
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+void Draw()
+{
+	system("cls"); //Clear the screen.
+	for (int i = 0; i < width+2; i++) cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+				 cout << "#";
+			if (i == y && j == x)
+				cout << "O";
+			else if (i == fruitY && j == fruitX)
+				cout << "9";
+			else
+				cout << " ";
+			
+			if (j == width - 1) cout << "#";
+
+		}
+
+		cout << endl;
+		
+	}
+
+	for (int i = 0; i < width+2; i++) cout << "#";
+	cout << endl; 
+
+}
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+
+		}
+	}
+}
+void Logic()
+{   
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+	switch (dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	default:
+		break;
+	}
+	if (x > width || x < 0 || y > height || y < 0)
+	cout << "GAME OVER!!!";
+	//	gameOver = true;
+
+	if (x == fruitX && y == fruitY)
+	{
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		nTail++;
+	}cout << "Score: "<< score << endl;
+}
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+
+		Draw();
+		Input();
+		Logic();
+		Sleep(80);
+	}
+	return 0;
+}
+
+
